7b05c8ef06d7e1e6a469c2513bd7c69be147a892
3AM-Studios <3am.stu@gmail.com>
2025-01-12 16:50:14 -0600

    Initial Commit

---
+++ PPTAMinimal/PPTAMinimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPTAMinimalApp.swift
+//  PPTAMinimal
+//
+//  Created by Sungbin Yun on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PPTAMinimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PPTAMinimalTests/PPTAMinimalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PPTAMinimalTests.swift
+//  PPTAMinimalTests
+//
+//  Created by Sungbin Yun on 1/12/25.
+//
+
+import Testing
+@testable import PPTAMinimal
+
+struct PPTAMinimalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
