2e5643d6fc45d2edf51cdc8d017d240a6b088282
Damien <damienkoh2025@u.northwestern.edu>
2025-10-05 22:47:37 -0400

    allow for preview from TabNavigator too

--- PPTAMinimal/Views/TabNavigator.swift
+++ PPTAMinimal/Views/TabNavigator.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct TabNavigator: View {
+    private let previewMode: Bool
+    
+    init(previewMode: Bool = false) {
+        self.previewMode = previewMode
+    }
+    
     var body: some View {
         TabView {
-            HomeView()
+            HomeView(previewMode: previewMode)
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Home")
@@ -25,4 +31,12 @@ struct TabNavigator: View {
         .background(Color.white)
         .edgesIgnoringSafeArea(.all)
     }
+}
+
+#Preview("TabNavigator") {
+    let auth = AuthViewModel()
+    auth.currentUser = User(id: "preview-user", name: "Preview Name", email: "preview@example.com")
+    
+    return TabNavigator(previewMode: true)
+        .environmentObject(auth)
 }
